6ed022ac88ebede0e2d2b6fa01272ca61f32ba0b
Phat Hoang <hoangphat4510@gmail.com>
2023-05-10 19:08:05 +0700

    Add project files.

---
+++ 1670_ASM2/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _1670_ASM2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ 1670_ASM2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _1670_ASM2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

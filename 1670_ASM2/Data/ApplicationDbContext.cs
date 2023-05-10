dc1df62d055d1863242e98f3512e11dc1c3da95f
Phat Hoang <hoangphat4510@gmail.com>
2023-05-10 21:50:37 +0700

    modify applicationDbContext file

--- 1670_ASM2/Data/ApplicationDbContext.cs
+++ 1670_ASM2/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using _1670_ASM2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace _1670_ASM2.Data
@@ -9,5 +10,24 @@ namespace _1670_ASM2.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.Entity<Book_Author>().HasKey(ba => new
+            {
+                ba.BookId,
+                ba.AuthorId
+            });
+
+            builder.Entity<Book_Author>().HasOne(b => b.Book).WithMany(ba => ba.Books_Authors).HasForeignKey(b => b.BookId);
+            builder.Entity<Book_Author>().HasOne(b => b.Author).WithMany(ba => ba.Books_Authors).HasForeignKey(b => b.AuthorId);
+            base.OnModelCreating(builder); 
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Book_Author> Books_Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
     }
 }
